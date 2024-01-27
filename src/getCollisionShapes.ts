d723401cdf22c1ff2f10432f502fd28df977f819
Vitaly Turovsky <vital2580@icloud.com>
2024-01-28 02:14:24 +0530

    shapes regression

--- src/getCollisionShapes.ts
+++ src/getCollisionShapes.ts
@@ -1,12 +1,12 @@
 import { adoptBlockOrItemNamesFromLatest } from 'flying-squid/dist/blockRenames'
 import collisionShapesInit from '../generated/latestBlockCollisionsShapes.json'
 import outputInteractionShapesJson from './interactionShapesGenerated.json'
-import supportedVersions from './supportedVersions.mjs'
 
 // defining globally to be used in loaded data, not sure of better workaround
 window.globalGetCollisionShapes = (version) => {
   // todo use the same in resourcepack
-  const renamedBlocks = adoptBlockOrItemNamesFromLatest('blocks', Object.keys(collisionShapesInit.blocks), supportedVersions.at(-1)!, version)
+  const versionFrom = collisionShapesInit.version
+  const renamedBlocks = adoptBlockOrItemNamesFromLatest('blocks', Object.keys(collisionShapesInit.blocks), versionFrom, version)
   const collisionShapes = {
     ...collisionShapesInit,
     blocks: Object.fromEntries(Object.entries(collisionShapesInit.blocks).map(([, shape], i) => [renamedBlocks[i], shape]))
