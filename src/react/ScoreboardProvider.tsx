85b95327a24c6ce72a8d3f6636f1d2225b913c08
Vitaly Turovsky <vital2580@icloud.com>
2024-03-07 00:56:03 +0300

    fix listeners

--- src/react/ScoreboardProvider.tsx
+++ src/react/ScoreboardProvider.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import Scoreboard from './Scoreboard' 
+import { useMemo, useState } from 'react'
+import Scoreboard from './Scoreboard'
 import type { ScoreboardItems } from './Scoreboard'
 
 
@@ -8,28 +8,31 @@ export default function ScoreboardProvider () {
   const [items, setItems] = useState<ScoreboardItems>([])
   const [open, setOpen] = useState(false)
 
-  useEffect(() => {
-    bot.on('scoreboardCreated', (scoreboard) => {
-      setTitle(scoreboard.title)
-      setItems([...scoreboard.items])
-      setOpen(true)
-    })
-    bot.on('scoreboardTitleChanged', (scoreboard) => {
-      setTitle(scoreboard.title)
-    })
-    bot.on('scoreUpdated', (scoreboard, item) => {
-      setItems([...items, ...scoreboard.items])
-    })
-    bot.on('scoreRemoved', (scoreboard, item) => {
-      setItems([...items, ...scoreboard.items])
-    })
-    bot.on('scoreboardDeleted', (scoreboard) => {
-      setOpen(false)
+  useMemo(() => { // useMemo instead of useEffect to register them asap and not after the initial dom render
+    const updateSidebarScoreboard = () => {
+      if (bot.scoreboard.sidebar) {
+        setTitle(bot.scoreboard.sidebar.title)
+        setItems([...bot.scoreboard.sidebar.items])
+        setOpen(true)
+      } else {
+        setOpen(false)
+      }
+    }
+
+    bot.on('scoreboardCreated', updateSidebarScoreboard) // not used atm but still good to have
+    bot.on('scoreboardTitleChanged', updateSidebarScoreboard)
+    bot.on('scoreUpdated', updateSidebarScoreboard)
+    bot.on('scoreRemoved', updateSidebarScoreboard)
+    bot.on('scoreboardDeleted', updateSidebarScoreboard)
+    bot.on('scoreboardPosition', () => {
+      void Promise.resolve().then(() => {
+        updateSidebarScoreboard()
+      }) // mineflayer bug: wait for the next tick to get the correct scoreboard position
     })
   }, [])
 
   return(
-    <Scoreboard 
+    <Scoreboard
       title={title}
       items={items}
       open={open}
