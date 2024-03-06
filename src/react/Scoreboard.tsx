4bb5d695c88859a00581db1651ff60483b186320
gguio <nikvish150@gmail.com>
2024-03-07 00:52:37 +0400

    Tested on play.minemalia.com

--- src/react/Scoreboard.tsx
+++ src/react/Scoreboard.tsx
@@ -1,7 +1,8 @@
 import './Scoreboard.css'
+import MessageFormattedString from './MessageFormattedString'
 
 
-export type ScoreboardItems = Array<{name: string, value: number}>
+export type ScoreboardItems = Array<{name: string, value: number, displayName?: any}>
 
 type ScoreboardProps = {
   title: string,
@@ -9,21 +10,20 @@ type ScoreboardProps = {
   open: boolean
 }
 
-
 export default function Scoreboard ({ title, items, open }: ScoreboardProps) {
 
   if (!open) return null
   return (
     <div className='scoreboard-container'>
       <div className='scoreboard-title'>
-        {title}
+        <MessageFormattedString message={title} />
       </div>
       {
         items.map((item, index) => {
           return(
             <div key={index} className='item-container'>
               <div className='item-name'>
-                {item.name}
+                <MessageFormattedString message={item.displayName ?? item.name} />
               </div>
               <div className='item-value'>
                 {item.value}
--- src/react/ScoreboardProvider.tsx
+++ src/react/ScoreboardProvider.tsx
@@ -3,41 +3,28 @@ import Scoreboard from './Scoreboard'
 import type { ScoreboardItems } from './Scoreboard'
 
 
-
 export default function ScoreboardProvider () {
-  const [name, setName] = useState('scoreboard')
   const [title, setTitle] = useState('Scoreboard')
   const [items, setItems] = useState<ScoreboardItems>([])
   const [open, setOpen] = useState(false)
 
-  const updateScoreboard = (scoreboard, HandlerFunction) => {
-    if (scoreboard.name === name) HandlerFunction() 
-  }
-
   useEffect(() => {
     bot.on('scoreboardCreated', (scoreboard) => {
-      console.log(scoreboard)
       setTitle(scoreboard.title)
-      setItems(scoreboard.items)
-      setName(scoreboard.name) // name state doesnt change. Why ??
-      // to test locally:
-      // 1. bot._client.emit('scoreboard_objective', {action: 0, name: 'name', displayText: 'test'})
-      // 2. bot._client.emit('scoreboard_score', {action: 0, scoreName: 'name', itemName: 'item 1', value: 5})
+      setItems([...scoreboard.items])
       setOpen(true)
     })
     bot.on('scoreboardTitleChanged', (scoreboard) => {
-      updateScoreboard(scoreboard, () => {setTitle(scoreboard.title)})
+      setTitle(scoreboard.title)
     })
     bot.on('scoreUpdated', (scoreboard, item) => {
-      console.log(name)
-      console.log(scoreboard.name)
-      if (scoreboard.name === name) setItems([...items, ...scoreboard.items])
+      setItems([...items, ...scoreboard.items])
     })
     bot.on('scoreRemoved', (scoreboard, item) => {
-      if (scoreboard.name === name) setItems([...items, ...scoreboard.items])
+      setItems([...items, ...scoreboard.items])
     })
     bot.on('scoreboardDeleted', (scoreboard) => {
-      updateScoreboard(scoreboard, () => {setOpen(false)})
+      setOpen(false)
     })
   }, [])
 
