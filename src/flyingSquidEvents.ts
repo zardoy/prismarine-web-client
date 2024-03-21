dba4043b3e6346ccd54b941ac3244c6e54bab19c
Vitaly Turovsky <vital2580@icloud.com>
2024-03-21 10:28:32 +0300

    fix: disable useless second stats panel on ios

--- src/flyingSquidEvents.ts
+++ src/flyingSquidEvents.ts
@@ -1,9 +1,12 @@
+import { showModal } from './globalState'
+import { chatInputValueGlobal } from './react/ChatContainer'
 import { showNotification } from './react/NotificationProvider'
 
 export default () => {
     localServer!.on('warpsLoaded', () => {
       showNotification(`${localServer!.warps.length} Warps loaded`, 'Use /warp <name> to teleport to a warp point.', false, 'label-alt', () => {
-        // todo open warp command
+        chatInputValueGlobal.value = '/warp '
+        showModal({ reactType: 'chat' })
       })
     })
 }
