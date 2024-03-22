969e23c6a708aba78b27892a904ceef44c124c03
Vitaly Turovsky <vital2580@icloud.com>
2024-03-22 13:49:33 +0300

    prosemirror test

--- src/react/prosemirror-markdown.ts
+++ src/react/prosemirror-markdown.ts
@@ -9,6 +9,60 @@ export class ProseMirrorView {
   view
 
   constructor(target, content) {
+    const emDOM: DOMOutputSpec = ["em", 0], strongDOM: DOMOutputSpec = ["strong", 0], codeDOM: DOMOutputSpec = ["code", 0]
+    
+    const marks = {
+      /// A link. Has `href` and `title` attributes. `title`
+      /// defaults to the empty string. Rendered and parsed as an `<a>`
+      /// element.
+      link: {
+        attrs: {
+          href: {},
+          title: { default: null }
+        },
+        inclusive: false,
+        parseDOM: [{
+          tag: 'a[href]', getAttrs (dom: HTMLElement) {
+            return { href: dom.getAttribute('href'), title: dom.getAttribute('title') }
+          }
+        }],
+        toDOM (node) { const { href, title } = node.attrs; return ['a', { href, title }, 0] }
+      } as MarkSpec,
+
+      /// An emphasis mark. Rendered as an `<em>` element. Has parse rules
+      /// that also match `<i>` and `font-style: italic`.
+      em: {
+        parseDOM: [
+          { tag: 'i' }, { tag: 'em' },
+          { style: 'font-style=italic' },
+          { style: 'font-style=normal', clearMark: m => m.type.name === 'em' }
+        ],
+        toDOM () { return emDOM }
+      } as MarkSpec,
+
+      /// A strong mark. Rendered as `<strong>`, parse rules also match
+      /// `<b>` and `font-weight: bold`.
+      strong: {
+        parseDOM: [
+          { tag: 'strong' },
+          // This works around a Google Docs misbehavior where
+          // pasted content will be inexplicably wrapped in `<b>`
+          // tags with a font-weight normal.
+          { tag: 'b', getAttrs: (node: HTMLElement) => node.style.fontWeight != 'normal' && null },
+          { style: 'font-weight=400', clearMark: m => m.type.name === 'strong' },
+          { style: 'font-weight', getAttrs: (value: string) => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null },
+        ],
+        toDOM () { return strongDOM }
+      } as MarkSpec,
+      
+      // /// Code font mark. Represented as a `<code>` element.
+      // code: {
+      //   parseDOM: [{ tag: 'code' }],
+      //   toDOM () { return codeDOM }
+      // } as MarkSpec
+    }
+
+
     console.log('schema.marks', schema.marks)
     //@ts-expect-error
     schema.marks.textColor = {
