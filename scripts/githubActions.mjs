ac3448c74502e6e23c357c1614bc81dab3beb354
Vitaly <vital2580@icloud.com>
2024-04-09 03:47:03 +0300

    ci: set custom mcraft aliases for PRs! (#99)

--- scripts/githubActions.mjs
+++ scripts/githubActions.mjs
@@ -1,18 +1,27 @@
+//@ts-check
+import fs from 'fs'
+import os from 'os'
+
 const fns = {
   async getAlias () {
     const aliasesRaw = process.env.ALIASES
     if (!aliasesRaw) throw new Error('No aliases found')
-    const aliases = aliasesRaw.split('\n').map((x) => x.search('='))
+    const aliases = aliasesRaw.split('\n').map((x) => x.split('='))
     const githubActionsPull = process.env.GITHUB_REF.match(/refs\/pull\/(\d+)\/merge/)
     if (!githubActionsPull) throw new Error(`Not a pull request, got ${process.env.GITHUB_REF}`)
     const prNumber = githubActionsPull[1]
     const alias = aliases.find((x) => x[0] === prNumber)
     if (alias) {
-      console.log('Found alias', alias[1])
       // set github output
-      console.log(`::set-output name=alias::${alias[1]}`)
+      setOutput('alias', alias[1])
     }
   }
+}
+
+function setOutput(key, value) {
+  // Temporary hack until core actions library catches up with github new recommendations
+  const output = process.env['GITHUB_OUTPUT']
+  fs.appendFileSync(output, `${key}=${value}${os.EOL}`)
 }
 
 const fn = fns[process.argv[2]]
