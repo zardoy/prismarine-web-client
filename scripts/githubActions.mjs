33d563cf9488970fc4d4e485ec6a7441f8129d6b
Vitaly Turovsky <vital2580@icloud.com>
2024-04-10 04:19:22 +0300

    fix action

--- scripts/githubActions.mjs
+++ scripts/githubActions.mjs
@@ -8,7 +8,7 @@ const fns = {
     if (!aliasesRaw) throw new Error('No aliases found')
     const aliases = aliasesRaw.split('\n').map((x) => x.split('='))
     const githubActionsPull = process.env.PULL_URL?.split('/').at(-1)
-    if (!githubActionsPull) throw new Error(`Not a pull request, got ${process.env.GITHUB_REF}`)
+    if (!githubActionsPull) throw new Error(`Not a pull request, got ${process.env.PULL_URL}`)
     const prNumber = githubActionsPull[1]
     const alias = aliases.find((x) => x[0] === prNumber)
     if (alias) {
@@ -18,7 +18,7 @@ const fns = {
   }
 }
 
-function setOutput(key, value) {
+function setOutput (key, value) {
   // Temporary hack until core actions library catches up with github new recommendations
   const output = process.env['GITHUB_OUTPUT']
   fs.appendFileSync(output, `${key}=${value}${os.EOL}`)
