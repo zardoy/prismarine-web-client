662c823c23bffbaeb1f91ca68f5fb3a692836d8e
Vitaly Turovsky <vital2580@icloud.com>
2024-04-11 20:19:32 +0300

    [pick] fix alias set

--- scripts/githubActions.mjs
+++ scripts/githubActions.mjs
@@ -6,10 +6,10 @@ const fns = {
   async getAlias () {
     const aliasesRaw = process.env.ALIASES
     if (!aliasesRaw) throw new Error('No aliases found')
-    const aliases = aliasesRaw.split('\n').map((x) => x.split('='))
+    const aliases = aliasesRaw.split('\n').map((x) => x.trim().split('='))
     const githubActionsPull = process.env.PULL_URL?.split('/').at(-1)
     if (!githubActionsPull) throw new Error(`Not a pull request, got ${process.env.PULL_URL}`)
-    const prNumber = githubActionsPull[1]
+    const prNumber = githubActionsPull
     const alias = aliases.find((x) => x[0] === prNumber)
     if (alias) {
       // set github output
