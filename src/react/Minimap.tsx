6ebe049ad1d85183a64aa69f42b199a69f11eff7
gguio <nikvish150@gmail.com>
2024-06-20 18:24:12 +0400

    full map container

--- src/react/Minimap.tsx
+++ src/react/Minimap.tsx
@@ -1,41 +1,52 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import { MinimapDrawer } from './MinimapDrawer'
 
 export default () => {
+  const [fullMapOpened, setFullMapOpened] = useState(false)
   const canvasTick = useRef(0)
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const drawerRef = useRef<MinimapDrawer | null>(null)
 
   function updateMap () {
-    if (drawerRef.current && canvasTick.current % 10 === 0) {
-      console.log(drawerRef.current.worldColors)
+    if (drawerRef.current && canvasTick.current % 2 === 0) {
       drawerRef.current.draw(bot)
       if (canvasTick.current % 300 === 0) {
         drawerRef.current.deleteOldWorldColors(bot.entity.position.x, bot.entity.position.z)
-        console.log(drawerRef.current.worldColors)
       }
     }
     canvasTick.current += 1
   }
 
+  const openFullMap = () => {
+    setFullMapOpened(true)
+  }
+
   useEffect(() => {
-    if (canvasRef.current) {
+    if (canvasRef.current && !drawerRef.current) {
       drawerRef.current = new MinimapDrawer(canvasRef.current)
     }
   }, [canvasRef.current])
 
   useEffect(() => {
-    console.log('set update')
     bot.on('move', updateMap)
 
     return () => {
-      console.log('delete update')
       bot.off('move', updateMap)
     }
   }, [])
 
+  return fullMapOpened ? <div 
+    style={{
+      position: 'absolute',
+      inset: '0px',
+      display: 'flex',
+      justifyContent: 'center',
+      border: '2px solid red'
+    }}
+  >
+    <canvas width={300} height={300} ref={canvasRef}></canvas>
 
-  return <div
+  </div> : <div
     className='minimap'
     style={{
       position: 'absolute',
