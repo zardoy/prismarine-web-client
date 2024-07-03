7af85d8c11dca790f14b426b82d5911e1015dbd2
Vitaly Turovsky <vital2580@icloud.com>
2024-07-03 13:16:25 +0300

    make it work somehow

--- prismarine-viewer/viewer/lib/worldrendererThree.ts
+++ prismarine-viewer/viewer/lib/worldrendererThree.ts
@@ -63,8 +63,13 @@ export class WorldRendererThree extends WorldRendererCommon {
 
     addWater () {
         // offset 0, -2, 0
-        const pos = this.camera.position.clone().add(new THREE.Vector3(0, -2, 0))
-        const posChunkMiddle = pos.toArray().map(x => Math.floor(x / 16) * 16 + 8) as [number, number, number]
+        const chunkPos = new THREE.Vector3(...this.camera.position.clone().toArray().map(x => Math.floor(x / 16) * 16) as [number, number, number])
+        const x = 0
+        const z = 0
+        const y = this.camera.position.y - chunkPos[1]
+
+        // const posChunkMiddle = chunkPos.map((x, i) => i === 0 ? 0 : 0) as [number, number, number]
+        const posChunkMiddle = chunkPos.toArray()
         const waterGeometry = new THREE.PlaneGeometry(20, 20)
         const textureLoader = new THREE.TextureLoader()
         const water = new Water(waterGeometry, {
@@ -75,8 +80,8 @@ export class WorldRendererThree extends WorldRendererCommon {
             normalMap0: textureLoader.load('https://threejs.org/examples/textures/water/Water_1_M_Normal.jpg'),
             normalMap1: textureLoader.load('https://threejs.org/examples/textures/water/Water_2_M_Normal.jpg'),
         })
-        const scale = 20/16/16
-        water.scale.set(scale, scale, scale)
+        const scale = 20 / 16 / 16
+        water.scale.set(scale, scale, 1)
         //@ts-ignore
         water.material.uniforms['color'].value.set(new THREE.Color(117, 202, 255).getHex())
         water.rotation.x = Math.PI * - 0.5
