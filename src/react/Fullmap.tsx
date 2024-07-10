39d6405b9b95ae390e5620f72d9aabff2aae14e8
gguio <nikvish150@gmail.com>
2024-07-10 11:59:55 +0400

    fullmap in a separate component

--- src/react/Fullmap.tsx
+++ src/react/Fullmap.tsx
@@ -1,136 +1,70 @@
-import { useRef, useEffect, useState, CSSProperties, Dispatch, SetStateAction } from 'react'
 import { Vec3 } from 'vec3'
+import { useRef, useState, CSSProperties, Dispatch, SetStateAction } from 'react'
 import { WorldWarp } from 'flying-squid/dist/lib/modules/warps'
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
-import { showModal, hideModal } from '../globalState'
-import { useIsModalActive } from './utilsApp'
 import { MinimapDrawer, DrawerAdapter } from './MinimapDrawer'
-import Input from './Input'
 import Button from './Button'
-import Fullmap from './Fullmap'
+import Input from './Input'
 
 
-export default ({ adapter, fullMap }: { adapter: DrawerAdapter, fullMap?: boolean }) => {
-  const isDragging = useRef(false)
+type FullmapProps = {
+  onClick: () => void, 
+  adapter: any, 
+  drawer: MinimapDrawer | null,
+  canvasRef: any
+}
+
+export default ({ onClick, adapter, drawer, canvasRef }: FullmapProps) => {
   const zoomRef = useRef(null)
-  const fullMapOpened = useIsModalActive('full-map')
-  const full = useRef(false)
   const [isWarpInfoOpened, setIsWarpInfoOpened] = useState(false)
-  const canvasTick = useRef(0)
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-  const drawerRef = useRef<MinimapDrawer | null>(null)
 
-  const updateMap = () => {
-    if (drawerRef.current) {
-      drawerRef.current.draw(adapter.playerPosition, undefined, full.current)
-      if (!full.current) {
-        rotateMap()
-      }
-      if (canvasTick.current % 300 === 0) {
-        drawerRef.current.deleteOldWorldColors(adapter.playerPosition.x, adapter.playerPosition.z)
-      }
-    }
-    canvasTick.current += 1
-  }
-
-  const toggleFullMap = () => {
-    if (fullMapOpened) {
-      hideModal({ reactType: 'full-map' })
-      full.current = false
-    } else {
-      showModal({ reactType: 'full-map' })
-      full.current = true
-    }
-  }
-
-  const eventControl = (event: MouseEvent | TouchEvent) => {
-    if (event.type === 'mousemove' || event.type === 'touchmove') {
-      isDragging.current = true
-    }
-
-    if (event.type === 'mouseup' || event.type === 'touchend') {
-      if (!isDragging.current) {
-        handleClickOnMap(event)
-      }
-      isDragging.current = false
-    }
-  }
-
-  const handleClickOnMap = (e: MouseEvent | TouchEvent) => {
-    drawerRef.current?.setWarpPosOnClick(e, adapter.playerPosition)
-    setIsWarpInfoOpened(true)
-  }
-
-  const updateWarps = () => { }
-
-  const rotateMap = () => {
-    if (!drawerRef.current) return
-    const angle = adapter.yaw % (Math.PI * 2)
-    drawerRef.current.canvas.style.transform = `rotate(${angle}rad)`
-  }
-
-  useEffect(() => {
-    if (canvasRef.current && !drawerRef.current) {
-      drawerRef.current = new MinimapDrawer(canvasRef.current, adapter)
-    } else if (canvasRef.current && drawerRef.current) {
-      drawerRef.current.canvas = canvasRef.current
-    }
-  }, [canvasRef.current, fullMapOpened, fullMap])
-
-  useEffect(() => {
-    if ((fullMapOpened || fullMap) && canvasRef.current) {
-      canvasRef.current.addEventListener('mousemove', eventControl)
-      canvasRef.current.addEventListener('touchmove', eventControl)
-      canvasRef.current.addEventListener('mouseup', eventControl)
-      canvasRef.current.addEventListener('touchend', eventControl)
-    } else if (!fullMapOpened || !fullMap) {
-      setIsWarpInfoOpened(false)
-    }
-
-    return () => {
-      canvasRef.current?.removeEventListener('mousemove', eventControl)
-      canvasRef.current?.removeEventListener('touchmove', eventControl)
-      canvasRef.current?.removeEventListener('mouseup', eventControl)
-      canvasRef.current?.removeEventListener('touchend', eventControl)
-    }
-  }, [fullMapOpened, fullMap])
-
-  useEffect(() => {
-    adapter.on('updateMap', updateMap)
-    adapter.on('toggleFullMap', toggleFullMap)
-    adapter.on('updateWaprs', updateWarps)
-
-    return () => {
-      adapter.off('updateMap', updateMap)
-      adapter.off('toggleFullMap', toggleFullMap)
-      adapter.off('updateWaprs', updateWarps)
-    }
-  }, [adapter])
-
-  return fullMapOpened || fullMap ? <Fullmap
-    onClick={()=>{
-      toggleFullMap()
+  return <div
+    style={{
+      position: 'absolute',
+      isolation: 'isolate',
+      inset: '0px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      border: '2px solid red',
+      backgroundColor: 'rgba(0, 0, 0, 0.4)'
     }}
-    adapter={adapter}
-    drawer={drawerRef.current}
-    canvasRef={canvasRef}
-  /> 
-    : <div
-      className='minimap'
+  >
+    <div
       style={{
         position: 'absolute',
-        right: '0px',
-        top: '0px',
-        padding: '5px 5px 0px 0px',
-        border: '2px solid red'
+        width: '100%',
+        height: '100%',
+        zIndex: '-1'
       }}
-      onClick={() => {
-        toggleFullMap()
+      onClick={onClick}
+    ></div>
+
+    <TransformWrapper
+      limitToBounds={false}
+      ref={zoomRef}
+      minScale={0.1}    
+      doubleClick={{
+        disabled: true
       }}
     >
-      <canvas width={80} height={80} ref={canvasRef}></canvas>
-    </div>
+      <TransformComponent
+        wrapperStyle={{
+          border: '1px solid black',
+          willChange: 'transform',
+        }}
+      >
+        <canvas
+          width={200}
+          height={200}
+          ref={canvasRef}
+        ></canvas>
+      </TransformComponent>
+    </TransformWrapper>
+    {isWarpInfoOpened && <WarpInfo adapter={adapter} drawer={drawer} setIsWarpInfoOpened={setIsWarpInfoOpened} />}
+  </div>
 }
+
 
 const WarpInfo = (
   { adapter, drawer, setIsWarpInfoOpened }
