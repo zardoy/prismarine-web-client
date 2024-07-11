9c70449d8618202609222e43b2b97d6460a814ef
soulless-ai <mr.chupak@gmail.com>
2024-07-11 23:04:43 +0500

    Fix ESlint issues in Book component

--- src/react/Book.stories.tsx
+++ src/react/Book.stories.tsx
@@ -1,14 +1,14 @@
-import { Meta, Story } from '@storybook/react';
-import Book, { BookProps } from './Book';
+import { Meta, Story } from '@storybook/react'
+import Book, { BookProps } from './Book'
 
 export default {
   title: 'Components/Book',
   component: Book,
-} as Meta;
+} as Meta
 
-const Template: Story<BookProps> = (args) => <Book {...args} />;
+const Template: Story<BookProps> = (args) => <Book {...args} />
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 Default.args = {
   textPages: [
     'Page 1: This is some text for page 1.',
@@ -20,4 +20,4 @@ Default.args = {
   editable: true,
   onSign: (pages) => console.log('Signed with pages:', pages),
   onClose: () => console.log('Closed book'),
-};
+}
