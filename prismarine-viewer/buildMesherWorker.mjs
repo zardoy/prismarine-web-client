d358c40d996c901787e98510d7bc1b0386ac1842
Vitaly Turovsky <vital2580@icloud.com>
2024-07-19 15:14:04 +0300

    fix mkdir

--- prismarine-viewer/buildMesherWorker.mjs
+++ prismarine-viewer/buildMesherWorker.mjs
@@ -116,8 +116,9 @@ const buildOptions = {
                 // skip writing & browser loading sourcemap there, worker debugging should be done in playground
                 // continue
               }
-              fs.mkdirSync(outDir, { recursive: true })
-              fs.writeFileSync(path.join(__dirname, outDir, path.basename(outputFile.path)), outputFile.text)
+              const writePath = path.join(__dirname, outDir, path.basename(outputFile.path))
+              fs.mkdirSync(path.dirname(writePath), { recursive: true })
+              fs.writeFileSync(writePath, outputFile.text)
             }
           }
         })
