f22b9b5bd39abbb3b2760c8b6426d6980d1e8881
gguio <nikvish150@gmail.com>
2024-08-15 12:48:24 +0400

    component integrated

--- src/react/Select.tsx
+++ src/react/Select.tsx
@@ -1,9 +1,8 @@
-import { omitObj } from '@zardoy/utils'
-import { AutocompleteChangeReason, useAutocomplete } from '@mui/base'
 import { useState, CSSProperties, SyntheticEvent, useRef, useEffect } from 'react'
-import PixelartIcon from './PixelartIcon'
+import Select, { CSSObjectWithLabel } from 'react-select'
 import Input from './Input'
 import './Select.css'
+import styles from './select.module.css'
 
 
 export interface OptionsStorage {
@@ -16,7 +15,7 @@ interface Props {
   updateOptions: (options: OptionsStorage) => void
   processInput?: (input: string) => CSSProperties | undefined
   processOption?: (option: string) => string
-  onChange?: (event: SyntheticEvent, value: string | null, reason: AutocompleteChangeReason) => void
+  onValueChange?: (newVal: string) => void
   iconInput?: string
   iconOption?: string
   containerStyle?: CSSProperties
@@ -28,103 +27,55 @@ export default ({
   updateOptions,
   processOption,
   processInput,
-  onChange,
+  onValueChange,
   iconInput,
   iconOption,
   containerStyle,
-  inputProps
 }: Props) => {
   const [inputValue, setInputValue] = useState('')
-  const [options, setOptions] = useState(initialOptions)
   const [inputStyle, setInputStyle] = useState<CSSProperties>({})
 
-  const autocomplete = useAutocomplete({
-    value: options.selected,
-    options: options.options.filter(option => option !== options.selected),
-    onChange,
-    onInputChange (event, value, reason) {
-      setInputStyle(processInput?.(value) ?? {})
-      setInputValue(value)
-      if (value) {
-        updateOptions({
-          ...options,
-          selected: value
-        })
-        setOptions({
-          ...options,
-          selected: value
-        })
+  const options = initialOptions.options.map(opt => { return { value: opt, label: opt } })
+
+  return <Select
+    options={options}
+    aria-invalid={'true'}
+    hideSelectedOptions={true}
+    isClearable={true}
+    onChange={(e, action) => {
+      console.log('value:', e?.value)
+      setInputValue(e?.label ?? '')
+      onValueChange?.(e?.value ?? '')
+      setInputStyle(processInput?.(e?.value ?? '') ?? {})
+    }}
+    classNames={{
+      control(state) {
+        return styles.container
+      },
+      input(state) {
+        return styles.input
+      },
+      option(state) {
+        return styles.container
       }
-    },
-    freeSolo: true,
-    openOnFocus: true
-  })
-
-  return <div {...autocomplete.getRootProps()} style={{ width: 130, position: 'relative', ...containerStyle }}>
-    <SelectOption
-      {...omitObj(autocomplete.getInputProps(), 'ref')}
-      inputRef={autocomplete.getInputProps().ref as any}
-      value={inputValue}
-      inputStyle={inputStyle}
-      inputProps={inputProps}
-      option={autocomplete.inputValue ?? ''}
-      icon={iconInput ?? ''}
-    />
-    {autocomplete.groupedOptions && <ul {...autocomplete.getListboxProps()} style={{
-      position: 'absolute',
-      zIndex: 10,
-      maxHeight: '100px',
-      overflow: 'auto',
-      width: containerStyle?.width ?? '130px'
-    }}>
-      {autocomplete.groupedOptions.map((option, index) => {
-        const { itemRef, ...optionProps } = autocomplete.getOptionProps({ option, index })
-        const optionString = processOption?.(option) ?? option
-        return <SelectOption {...optionProps as any} icon={iconOption ?? ''} option={optionString} />
-      })}
-    </ul>}
-  </div>
+    }}
+    styles={{
+      control(base, state) { return { ...containerStyle, ...inputStyle } },
+      menu(base, state) { return {} },
+      option(base, state) {
+        return {
+          boxSizing: 'border-box',
+          padding: '2px 8px',
+          backgroundColor: state.isFocused ? '#4a4646' : 'black',
+          ...containerStyle
+        }
+      },
+      input(base, state) { return {} },
+      indicatorsContainer(base, state) { return { display: 'none' } },
+      placeholder(base, state) { return { ...base, position: 'absolute' } },
+      singleValue(base, state) { return { ...base, margin: '0px', position: 'absolute', color: 'white' } },
+      noOptionsMessage(base, state) { return { display: 'none' } }
+    }}
+  />
 }
 
-const SelectOption = ({ option, inputRef, icon, inputStyle, inputProps, value, setValue, ...props }: {
-  option: string,
-  icon?: string,
-  inputProps?: React.ComponentProps<typeof Input>
-  inputStyle?: CSSProperties
-} & Record<string, any>) => {
-  const [firstRender, setFirstRender] = useState(true)
-
-  useEffect(() => {
-    if (firstRender) setFirstRender(false)
-    if (value === '' && !firstRender) props.onFocus?.()
-  }, [value])
-
-  return <div style={{
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
-  }}
-  {...omitObj(props, 'id')}
-
-  >
-    {icon && <PixelartIcon styles={{ position: 'absolute', zIndex: 3 }} iconName={icon} />}
-    <Input
-      id={props.id}
-      inputRef={inputRef}
-      style={{
-        paddingLeft: icon ? 16 : 5,
-        width: '100%',
-        // textOverflow: 'ellipsis',
-        left: '0px',
-      }}
-      rootStyles={{
-        width: '99%',
-        border: '1px solid grey',
-        ...inputStyle
-      }}
-      value={option}
-      onChange={props.onChange}
-      {...inputProps}
-    />
-  </div>
-}
