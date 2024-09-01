8fd2eccd30962f243e016fd445b359341853dfb3
Vitaly <vital2580@icloud.com>
2024-09-01 17:42:26 +0300

    Update entities.ts

--- src/entities.ts
+++ src/entities.ts
@@ -97,7 +97,7 @@ customEvents.on('gameLoaded', () => {
   })
 
   bot._client.on('entity_status', (data) => {
-    if (versionToNumber(bot.version) >= versionToNumber("1.19.4")) return
+    if (versionToNumber(bot.version) >= versionToNumber('1.19.4')) return
     const { entityId, entityStatus } = data
     if (entityStatus === 2 && viewer.entities.entities[entityId]) {
       viewer.entities.handleDamageEvent(entityId, entityStatus)
