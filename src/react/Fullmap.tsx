c707d095875b3668a6cce50008425604480e624f
gguio <nikvish150@gmail.com>
2024-09-23 12:54:57 +0400

    fix color picker for ios

--- src/react/Fullmap.tsx
+++ src/react/Fullmap.tsx
@@ -455,6 +455,9 @@ const WarpInfo = (
             if (!e.target) return
             setWarp(prev => { return { ...prev, color: e.target.value } })
           }}
+          rootStyles={{
+            width: /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream ? '20px' : '200px',
+          }}
         />
       </div>
       <div style={fieldCont} >
