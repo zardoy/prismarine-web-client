049e6c46bb6ff4862174e499df18a2dc2b3fffee
Vitaly Turovsky <vital2580@icloud.com>
2024-10-30 23:53:45 +0300

    fix: don't make text move in loading screens

--- src/react/AppStatus.tsx
+++ src/react/AppStatus.tsx
@@ -14,27 +14,26 @@ export default ({
   actionsSlot = null as React.ReactNode | null,
   children
 }) => {
-  const [loadingDots, setLoadingDots] = useState('')
+  const [loadingDotIndex, setLoadingDotIndex] = useState(0)
 
   useEffect(() => {
+    const statusRunner = async () => {
+      const timer = async (ms) => new Promise((resolve) => { setTimeout(resolve, ms) })
+
+      const load = async () => {
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+          setLoadingDotIndex(i => (i + 1) % 4)
+          await timer(500) // eslint-disable-line no-await-in-loop
+        }
+      }
+
+      void load()
+    }
+
     void statusRunner()
   }, [])
 
-  const statusRunner = async () => {
-    const array = ['.', '..', '...', '']
-
-    const timer = async (ms) => new Promise((resolve) => { setTimeout(resolve, ms) })
-
-    const load = async () => {
-      // eslint-disable-next-line no-constant-condition
-      for (let i = 0; true; i = (i + 1) % array.length) {
-        setLoadingDots(array[i])
-        await timer(500) // eslint-disable-line no-await-in-loop
-      }
-    }
-
-    void load()
-  }
 
   return (
     <Screen
@@ -48,7 +47,16 @@ export default ({
           >
             {status}
           </span>
-          {isError || hideDots ? '' : loadingDots}
+          <div style={{ display: 'inline-flex', gap: '1px', }} hidden={hideDots || isError}>
+            {
+              [...'...'].map((dot, i) => {
+                return <span
+                  key={i} style={{
+                    visibility: loadingDotIndex <= i ? 'hidden' : 'visible',
+                  }}>{dot}</span>
+              })
+            }
+          </div>
           <p className={styles['potential-problem']}>{description}</p>
           <p className={styles['last-status']}>{lastStatus ? `Last status: ${lastStatus}` : lastStatus}</p>
         </>
