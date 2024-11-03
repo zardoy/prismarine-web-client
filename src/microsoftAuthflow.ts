dcc0960d7fe1a9d51bc7873b4608ae36b6e6a85c
Vitaly Turovsky <vital2580@icloud.com>
2024-11-04 02:51:37 +0300

    fix the auth check

--- src/microsoftAuthflow.ts
+++ src/microsoftAuthflow.ts
@@ -82,7 +82,7 @@ export default async ({ tokenCaches, proxyBaseUrl, setProgressText = (text) => {
       })
       if (!window.crypto && !isPageSecure()) throw new Error('Crypto API is available only in secure contexts. Be sure to use https!')
       const restoredData = await restoreData(result)
-      if (!restoredData?.certificates?.profileKeys?.private) {
+      if (!restoredData?.certificates?.profileKeys?.privatePEM) {
         throw new Error(`Failed to restore profile keys in ${JSON.stringify(result)}: ${JSON.stringify(restoredData)}`)
       }
       restoredData.certificates.profileKeys.private = restoredData.certificates.profileKeys.privatePEM
