8f69b60a41a65b39844e4f3f4d236302c5ff3149
Vitaly Turovsky <vital2580@icloud.com>
2024-11-05 07:10:57 +0300

    hotfix: disable fly after going out of spectator mode

--- src/controls.ts
+++ src/controls.ts
@@ -639,6 +639,7 @@ const endFlying = (sendAbilities = true) => {
 let allowFlying = false
 
 export const onBotCreate = () => {
+  let wasSpectatorFlying = false
   bot._client.on('abilities', ({ flags }) => {
     if (flags & 2) { // flying
       toggleFly(true, false)
@@ -650,6 +651,10 @@ export const onBotCreate = () => {
   const gamemodeCheck = () => {
     if (bot.game.gameMode === 'spectator') {
       toggleFly(true, false)
+      wasSpectatorFlying = true
+    } else if (wasSpectatorFlying) {
+      toggleFly(false, false)
+      wasSpectatorFlying = false
     }
   }
   bot.on('game', () => {
