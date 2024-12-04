d1d25eaa3a286fd198849e40370b8bb6b393218c
Vitaly Turovsky <vital2580@icloud.com>
2024-12-04 04:59:25 +0300

    [skip ci] move reporter below

--- prismarine-viewer/viewer/lib/worldrendererWebgpu.ts
+++ prismarine-viewer/viewer/lib/worldrendererWebgpu.ts
@@ -6,54 +6,6 @@ import { WorldRendererCommon } from './worldrendererCommon'
 import { MesherGeometryOutput } from './mesher/shared'
 import { updateStatText } from './ui/newStats'
 
-class RendererProblemReporter {
-  dom = document.createElement('div')
-  contextlostDom = document.createElement('div')
-  mainIssueDom = document.createElement('div')
-
-  constructor () {
-    document.body.appendChild(this.dom)
-    this.dom.className = 'renderer-problem-reporter'
-    this.dom.appendChild(this.contextlostDom)
-    this.dom.appendChild(this.mainIssueDom)
-    this.dom.style.fontFamily = 'monospace'
-    this.dom.style.fontSize = '20px'
-    this.contextlostDom.style.cssText = `
-      position: fixed;
-      top: 0;
-      left: 0;
-      right: 0;
-      color: red;
-      display: flex;
-      justify-content: center;
-      z-index: -1;
-      font-size: 18px;
-      text-align: center;
-    `
-    this.mainIssueDom.style.cssText = `
-      position: fixed;
-      inset: 0;
-      color: red;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      z-index: -1;
-      text-align: center;
-    `
-    this.reportProblem(false, 'Waiting for renderer...')
-    this.mainIssueDom.style.color = 'white'
-  }
-
-  reportProblem (isContextLost: boolean, message: string) {
-    this.mainIssueDom.style.color = 'red'
-    if (isContextLost) {
-      this.contextlostDom.textContent = `Renderer context lost (try restarting the browser): ${message}`
-    } else {
-      this.mainIssueDom.textContent = message
-    }
-  }
-}
-
 export class WorldRendererWebgpu extends WorldRendererCommon {
   outputFormat = 'webgpu' as const
   // webglData: WebglData
@@ -140,5 +92,53 @@ export class WorldRendererWebgpu extends WorldRendererCommon {
   //   for (let y = this.worldConfig.minY; y < this.worldConfig.worldHeight; y += 16) {
   //     webgpuChannel.removeBlocksSection(`${x},${y},${z}`)
   //   }
+  }
+}
+
+class RendererProblemReporter {
+  dom = document.createElement('div')
+  contextlostDom = document.createElement('div')
+  mainIssueDom = document.createElement('div')
+
+  constructor () {
+    document.body.appendChild(this.dom)
+    this.dom.className = 'renderer-problem-reporter'
+    this.dom.appendChild(this.contextlostDom)
+    this.dom.appendChild(this.mainIssueDom)
+    this.dom.style.fontFamily = 'monospace'
+    this.dom.style.fontSize = '20px'
+    this.contextlostDom.style.cssText = `
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      color: red;
+      display: flex;
+      justify-content: center;
+      z-index: -1;
+      font-size: 18px;
+      text-align: center;
+    `
+    this.mainIssueDom.style.cssText = `
+      position: fixed;
+      inset: 0;
+      color: red;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: -1;
+      text-align: center;
+    `
+    this.reportProblem(false, 'Waiting for renderer...')
+    this.mainIssueDom.style.color = 'white'
+  }
+
+  reportProblem (isContextLost: boolean, message: string) {
+    this.mainIssueDom.style.color = 'red'
+    if (isContextLost) {
+      this.contextlostDom.textContent = `Renderer context lost (try restarting the browser): ${message}`
+    } else {
+      this.mainIssueDom.textContent = message
+    }
   }
 }
