9dad509bc2ad724c0359168d92caa6f9d3aff6a6
Vitaly Turovsky <vital2580@icloud.com>
2024-12-20 23:18:16 +0300

    feat: improve main menu and sign in UIs. Add QR code so you can login to MS account on mobile

--- src/react/Screen.tsx
+++ src/react/Screen.tsx
@@ -5,14 +5,15 @@ interface Props {
   style?: React.CSSProperties
   className?: string
   titleSelectable?: boolean
+  titleMarginTop?: number
 }
 
-export default ({ title, children, backdrop = true, style, className, titleSelectable }: Props) => {
+export default ({ title, children, backdrop = true, style, className = '', titleSelectable, titleMarginTop }: Props) => {
   return (
     <>
       {backdrop === 'dirt' ? <div className='dirt-bg' /> : backdrop ? <div className="backdrop" /> : null}
       <div className={`fullscreen ${className}`} style={{ overflow: 'auto', ...style }}>
-        <div className="screen-content">
+        <div className="screen-content" style={titleMarginTop === undefined ? {} : { marginTop: titleMarginTop }}>
           <div className={`screen-title ${titleSelectable ? 'text-select' : ''}`}>{title}</div>
           {children}
         </div>
