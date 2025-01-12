c070fe836fc13ee017b0c855527d59409fb0e01e
Vitaly Turovsky <vital2580@icloud.com>
2025-01-12 12:58:36 +0700

    fix type regression

--- src/supportedVersions.mjs
+++ src/supportedVersions.mjs
@@ -2,6 +2,7 @@ import { supportedVersions, postNettyVersionsByProtocolVersion } from 'minecraft
 
 export const ignoredVersionsRegex = /(^0\.30c$)|w|-pre|-rc/
 
+/** @type {string[]} */
 const versionsFromProtocol = Object.values(postNettyVersionsByProtocolVersion.pc).flat().filter(x => !ignoredVersionsRegex.test(x.minecraftVersion)).map(x => x.minecraftVersion)
 
 export default versionsFromProtocol.filter(x => x !== '1.7' && !x.startsWith('1.7.'))
